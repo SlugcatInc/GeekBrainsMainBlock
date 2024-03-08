f5eed5663eb2dc6d603981c2a002a33e2a165159
Ivan Davydkin <Ivandavydkin91242@gmail.com>
2024-03-08 15:45:41 +0500

    Creating File

---
+++ Work/MainBlock.cs
@@ -0,0 +1,17 @@
+
+using System;
+
+public class MainBlock
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
