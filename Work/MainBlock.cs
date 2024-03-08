f89613240ddfcaa30c76630baabb9916f54cc865
Ivan Davydkin <Ivandavydkin91242@gmail.com>
2024-03-08 15:48:26 +0500

    Added Script

--- Work/MainBlock.cs
+++ Work/MainBlock.cs
@@ -1,17 +1,42 @@
-
 using System;
 
 public class MainBlock
 {
-    // Start is called before the first frame update
-    void Start()
+    static void Main()
     {
-        
+        string[] inputArray = { "Hello", "2", "world", ":-)" };
+
+        string[] resultArray = FilterArray(inputArray);
+
+        Console.WriteLine("Filtered array:");
+        foreach (string item in resultArray)
+        {
+            Console.WriteLine(item);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    static string[] FilterArray(string[] inputArray)
     {
-        
+        int count = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] resultArray = new string[count];
+        int index = 0;
+        foreach (string item in inputArray)
+        {
+            if (item.Length <= 3)
+            {
+                resultArray[index] = item;
+                index++;
+            }
+        }
+
+        return resultArray;
     }
 }
